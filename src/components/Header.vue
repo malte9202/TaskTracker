794093132d7ee09aedf7aaa872abbd83c8fcb814
malte9202 <malte.niedereichholz@gmail.com>
2021-12-22 20:03:50 +0100

    final commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button 
+        <Button v-show="homePage"
         @btn-click="$emit('toggle-add-task')" 
         :text="showAddTask ? 'Close' : 'Add Task'"  
         :color="showAddTask ? 'red' : 'green'" />
@@ -18,7 +18,16 @@ export default {
         showAddTask: Boolean,
     },
     components: {
-        Button
+        Button,
+    },
+    computed: {
+        homePage() {
+            if (this.$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
+        }
     }
 }
 </script>
